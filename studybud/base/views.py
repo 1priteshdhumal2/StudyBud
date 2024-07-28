06dcb459d6153ff5add801a79604f3707e80487c
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-07-28 23:21:15 +0530

    Added delete route and improve ui

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -11,8 +11,11 @@ from .forms import RoomForm
 # ]
 
 def home(request):
+    q = request.GET.get('q')
     rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -43,3 +46,11 @@ def updateRoom(request, pk):
 
     context = {'form': form}
     return render (request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id = pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('Home')
+    return render(request, 'base/delete.html', {'obj' : room })
