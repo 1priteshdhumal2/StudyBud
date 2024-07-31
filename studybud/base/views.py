783638fc790dcde128f9a3d3df4c34990de59f33
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-07-31 23:54:26 +0530

    Added navbar in project

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -11,8 +11,8 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    q = request.GET.get('q')
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__contains= q)
     topics = Topic.objects.all()
 
     context = {'rooms': rooms, 'topics': topics}
