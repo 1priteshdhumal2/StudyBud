238b4a37ed2de80ac6e31af355997086cac6d075
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-08-02 01:19:59 +0530

    Added authentication using django authentication

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.loginPage, name="login"),
-    path('', views.home, name="Home"),
-    path('room/<str:pk>/', views.room, name="Room"),
-    path('create-room/', views.createRoom , name="create-room"),
-    path('update-room/<str:pk>/', views.updateRoom , name="update-room"),
-    path('delete-room/<str:pk>/', views.deleteRoom , name="delete-room")
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("", views.home, name="Home"),
+    path("room/<str:pk>/", views.room, name="Room"),
+    path("create-room/", views.createRoom, name="create-room"),
+    path("update-room/<str:pk>/", views.updateRoom, name="update-room"),
+    path("delete-room/<str:pk>/", views.deleteRoom, name="delete-room"),
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -24,8 +27,21 @@ def loginPage(request):
         except:
             messages.error(request, "User does not exist")
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("Home")
+        else:
+            messages.error(request, "Username or Password does not exist")
+
     context = {}
     return render(request, "base/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("Home")
 
 
 def home(request):
@@ -48,6 +64,7 @@ def room(request, pk):
     return render(request, "base/room.html", context)
 
 
+@login_required(login_url="login")
 def createRoom(request):
     form = RoomForm
     if request.method == "POST":
@@ -60,9 +77,13 @@ def createRoom(request):
     return render(request, "base/room_form.html", context)
 
 
+@login_required(login_url="login")
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse("You are not allowed here!!")
 
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
@@ -74,8 +95,12 @@ def updateRoom(request, pk):
     return render(request, "base/room_form.html", context)
 
 
+@login_required(login_url="login")
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse("You are not allowed here!!")
 
     if request.method == "POST":
         room.delete()
