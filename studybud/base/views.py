44c8724037f8747f68371d2d6b86ca6d04761fe0
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-08-03 19:21:53 +0530

    Changing the UI of the website

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -71,14 +71,15 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
 
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count, "room_messages": room_messages}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    roomMessages = room.message_set.all().order_by('-created')
+    roomMessages = room.message_set.all()
     participants = room.participants.all()
 
     if request.method == 'POST':
@@ -93,6 +94,14 @@ def room(request, pk):
     context = {"room": room, "roomMessages" : roomMessages, "participants":participants}
     return render(request, "base/room.html", context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, "base/profile.html", context)
+
 
 @login_required(login_url="login")
 def createRoom(request):
@@ -100,7 +109,9 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect("Home")
 
     context = {"form": form}
@@ -136,3 +147,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect("Home")
     return render(request, "base/delete.html", {"obj": room})
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("You are not allowed here!!")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect("Home")
+    return render(request, "base/delete.html", {"obj": message})
