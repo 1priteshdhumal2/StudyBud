a831bc6870767dfb80f49ec70cc5240a084bfbdc
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-08-06 00:52:18 +0530

    Improve the UI for the login and update page

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
 from .forms import RoomForm
+from .forms import UserForm
 
 # Create your views here.
 
@@ -180,4 +181,13 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-    return render(request, 'base/update-user.html')
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST , instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk = user.id) 
+
+    return render(request, 'base/update-user.html', {'form' : form})
